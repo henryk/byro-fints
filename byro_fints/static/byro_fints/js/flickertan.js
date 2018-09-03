05bd976f5d089384a216b0e748b731f29091a9ee
Henryk Plötz <henryk@ploetzli.ch>
2018-09-03 20:29:54 +0200

    Implement fundamental Javascript controls

--- byro_fints/static/byro_fints/js/flickertan.js
+++ byro_fints/static/byro_fints/js/flickertan.js
@@ -3,6 +3,7 @@ $(function(){
         var interval = 25;
         var data = $(fdiv).data('flicker-content');
         var main = $(fdiv).find('.flicker-main');
+        var base_font_size = parseFloat( $(fdiv).css('font-size') );
 
         var outstream = ['1', '0', '31', '30', '31', '30'];
         for(var i=0; i<data.length; i++) {
@@ -11,8 +12,13 @@ $(function(){
             outstream.push(d << 1);
         }
         var index = 0;
+        var running = false;
 
         var update = function() {
+            if(!running) {
+                return;
+            }
+
             var output = outstream[index];
             index++;
             if(index >= outstream.length) {
@@ -25,9 +31,49 @@ $(function(){
         var start = function() {
             $(fdiv).removeClass('flicker-animate-css');
             $(fdiv).addClass('flicker-animate-js');
+            running = true;
+            $(fdiv).find('.flicker-control-playpause').removeClass('fa-play').addClass('fa-pause');
             update();
         };
-        //start();
+
+        var stop = function() {
+            running = false;
+            $(fdiv).find('.flicker-control-playpause').removeClass('fa-pause').addClass('fa-play');
+        };
+
+        $(fdiv).find('.flicker-control-speedminus').click(function(){
+            if(interval < 400) {
+                interval = (1000 / ((1000 / interval)-2.5));
+            };
+        });
+
+        $(fdiv).find('.flicker-control-speedplus').click(function(){
+            if(interval > 10) {
+                interval = (1000 / ((1000 / interval)+2.5));
+            };
+        });
+
+        $(fdiv).find('.flicker-control-zoomplus').click(function(){
+            $(fdiv).css('font-size',
+                parseFloat( $(fdiv).css('font-size') ) + (0.025*base_font_size)
+            );
+        });
+
+        $(fdiv).find('.flicker-control-zoomminus').click(function(){
+            $(fdiv).css('font-size',
+                Math.max( parseFloat( $(fdiv).css('font-size') ) - (0.025*base_font_size), 0.01*base_font_size)
+            );
+        });
+
+        $(fdiv).find('.flicker-control-playpause').click(function(){
+            if(running) {
+                stop();
+            } else {
+                start();
+            }
+        });
+
+        start();
     };
     $('.flicker-code').each(function(){Flicker(this)});
 });
