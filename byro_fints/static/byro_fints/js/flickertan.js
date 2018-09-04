ac7791e7b65c2f3f1fc1b19794bc5ec74f4f6e58
Henryk Plötz <henryk@ploetzli.ch>
2018-09-05 00:27:42 +0200

    Flickercode: Javascript settings (speed/zoom plus/minus) and local storage

--- byro_fints/static/byro_fints/js/flickertan.js
+++ byro_fints/static/byro_fints/js/flickertan.js
@@ -41,28 +41,106 @@ $(function(){
             $(fdiv).find('.flicker-control-playpause').removeClass('fa-pause').addClass('fa-play');
         };
 
-        $(fdiv).find('.flicker-control-speedminus').click(function(){
-            if(interval < 400) {
-                interval = (1000 / ((1000 / interval)-2.5));
+        var store_settings = function() {
+            if(typeof(Storage) === "undefined") {
+                return;
+            }
+            var data = interval + "__" + $(fdiv).css('font-size');
+            localStorage.setItem("flickercode-prefs_default", data);
+            localStorage.setItem("flickercode-prefs_"+screen.width, data);
+        };
+
+        var load_settings = function() {
+            if(typeof(Storage) === "undefined") {
+                return;
+            }
+
+            var data = localStorage.getItem('flickercode-prefs_'+screen.width);
+            if(data === null) {
+                data = localStorage.getItem('flickercode-prefs_default');
+            }
+            if(data === null) {
+                return;
+            }
+
+            var prefs = data.split("__");
+            interval = parseFloat(prefs[0]);
+            $(fdiv).css('font-size', prefs[1]);
+        };
+
+        var control_done = false;
+        var control_clicked = false;
+
+        var control = function(op) {
+            if(op == '<') {
+                if(interval < 400) {
+                    interval = (1000 / ((1000 / interval)-2.5));
+                };
+            } else if(op == '>') {
+                if(interval > 10) {
+                    interval = (1000 / ((1000 / interval)+2.5));
+                };
+            } else if(op == '-') {
+                $(fdiv).css('font-size',
+                    Math.max( parseFloat( $(fdiv).css('font-size') ) - (0.025*base_font_size), 0.01*base_font_size)
+                );
+            } else if(op == '+') {
+                $(fdiv).css('font-size',
+                    parseFloat( $(fdiv).css('font-size') ) + (0.025*base_font_size)
+                );
+            }
+            store_settings();
+        };
+
+        var control_down = function(op) {
+            control_done = false;
+            control_interval = window.setInterval(function(){
+                control_done = true;
+                control(op)
+            }, 150);
+        };
+
+        var control_up = function(op) {
+            window.clearInterval(control_interval);
+        };
+
+        var control_click = function(op) {
+            if(!control_done) {
+                control(op);
             };
+            control_done = false;
+        }
+
+        $(fdiv).find('.flicker-control-speedminus').click(function(){
+            control_click('<');
+        }).mousedown(function(){
+            control_down('<');
+        }).mouseup(function(){
+            control_up('<');
         });
 
         $(fdiv).find('.flicker-control-speedplus').click(function(){
-            if(interval > 10) {
-                interval = (1000 / ((1000 / interval)+2.5));
-            };
+            control_click('>');
+        }).mousedown(function(){
+            control_down('>');
+        }).mouseup(function(){
+            control_up('>');
         });
 
         $(fdiv).find('.flicker-control-zoomplus').click(function(){
-            $(fdiv).css('font-size',
-                parseFloat( $(fdiv).css('font-size') ) + (0.025*base_font_size)
-            );
+            control_click('+');
+        }).mousedown(function(){
+            control_down('+');
+        }).mouseup(function(){
+            control_up('+');
         });
 
         $(fdiv).find('.flicker-control-zoomminus').click(function(){
-            $(fdiv).css('font-size',
-                Math.max( parseFloat( $(fdiv).css('font-size') ) - (0.025*base_font_size), 0.01*base_font_size)
-            );
+            control_click('-');
+        }).mousedown(function(){
+            control_down('-');
+        }).mouseup(function(){
+            control_up('-');
         });
 
         $(fdiv).find('.flicker-control-playpause').click(function(){
@@ -73,6 +151,7 @@ $(function(){
             }
         });
 
+        load_settings();
         start();
     };
     $('.flicker-code').each(function(){Flicker(this)});
